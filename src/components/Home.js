60d22d43fa25c5b0fefe221e5fb589ad704e7f30
Eliyas Hossain <eliyashossain700@gmail.com>
2022-07-28 21:12:14 +0600

    lesson 3 completed

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,19 +1,23 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-
+import React from "react";
+import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
 const Home = () => {
   return (
-    <div className="home container">
-      <h2>
+    <motion.div
+      animate={{ rotateZ: 360 }}
+      transition={{ ease: "easeOut", duration: 2 }}
+      className="home container"
+    >
+      <motion.h2 animate={{ fontSize: 55, color: "red", x: 20, y: -12 }}>
         Welcome to Pizza Joint
-      </h2>
+      </motion.h2>
       <Link to="/base">
-        <button>
+        <motion.button animate={{ scale: 1.5 }}>
           Create Your Pizza
-        </button>
+        </motion.button>
       </Link>
-    </div>
-  )
-}
+    </motion.div>
+  );
+};
 
 export default Home;
