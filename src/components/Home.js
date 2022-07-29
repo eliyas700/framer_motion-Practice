605448d96d447241e0e314a0c1f82ea722ea1bdf
Eliyas Hossain <eliyashossain700@gmail.com>
2022-07-29 11:13:55 +0600

    Framer Motion Initial

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,18 +3,10 @@ import { Link } from "react-router-dom";
 import { motion } from "framer-motion";
 const Home = () => {
   return (
-    <motion.div
-      animate={{ rotateZ: 360 }}
-      transition={{ ease: "easeOut", duration: 2 }}
-      className="home container"
-    >
-      <motion.h2 animate={{ fontSize: 55, color: "red", x: 20, y: -12 }}>
-        Welcome to Pizza Joint
-      </motion.h2>
+    <motion.div animate={{}} transition={{}} className="home container">
+      <motion.h2 animate={{}}>Welcome to Pizza Joint</motion.h2>
       <Link to="/base">
-        <motion.button animate={{ scale: 1.5 }}>
-          Create Your Pizza
-        </motion.button>
+        <motion.button animate={{}}>Create Your Pizza</motion.button>
       </Link>
     </motion.div>
   );
