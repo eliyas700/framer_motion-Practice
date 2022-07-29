4a88da62b9499c82014273e5dc92e7237cdcd858
Eliyas Hossain <eliyashossain700@gmail.com>
2022-07-29 11:38:38 +0600

    Transition Completed

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,8 +3,13 @@ import { Link } from "react-router-dom";
 import { motion } from "framer-motion";
 const Home = () => {
   return (
-    <motion.div animate={{}} transition={{}} className="home container">
-      <motion.h2 animate={{}}>Welcome to Pizza Joint</motion.h2>
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ delay: 1.5, duration: 1.5 }}
+      className="home container"
+    >
+      <motion.h2>Welcome to Pizza Joint</motion.h2>
       <Link to="/base">
         <motion.button animate={{}}>Create Your Pizza</motion.button>
       </Link>
